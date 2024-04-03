fe76a20a4c0183fa5135ab73954f99155d9c905d
Alexandre Stakonski <115736059+stakewalker@users.noreply.github.com>
2024-04-03 14:45:49 -0300

    Create timekeeper.py

---
+++ timekeeper.py
@@ -0,0 +1,10 @@
+# Keep listening to messages on Telegram group
+
+# If an image is sent, convert its contents to text and filter the time (hh:mm)
+# Otherwise if a message with "time" is received, use it
+
+# Print the data and ask user to confirm
+
+# Save this data to the Google Sheets file 
+
+# If more than 4 registers are found in one day, calculate how many hours has passed 
