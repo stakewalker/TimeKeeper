f151a107c8a03ffee58257c71e9bd8d9e109ce9a
Alexandre Stakonski <115736059+stakewalker@users.noreply.github.com>
2024-05-13 09:33:35 -0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -5,44 +5,44 @@ from telebot import types
 from datetime import datetime, timedelta
 
 
-def time_from_photo(img_obj):
-    # Open the image file
-    image = Image.open(img_obj)
-    # Extract text from the image
-    text = pytesseract.image_to_string(image)
-    # Look for time pattern (hh:mm)
-    time_pattern = re.compile(r'\b([01]?[0-9]|2[0-3]):[0-5][0-9]\b')
-    matches = time_pattern.findall(text)
-    return matches
+tg_bot = telebot.TeleBot(os.getenv('TG_BOT_TOKEN'))
+tg_group = os.getenv('TG_GROUP_ID')
 
-
-tl_bot = telebot.TeleBot(TL_BOT_TOKEN)
-@tl_bot.message_handler(func=lambda message: True, content_types=['text'])
+@tl_bot.message_handler(func=lambda message: message.chat.id == group_id_to_listen, content_types=['text', 'photo'])    
 def handle_message(message):
-    if message.chat.type == 'group':
-        # Create a markup with Yes and No buttons
-        markup = types.InlineKeyboardMarkup()
-        yes_button = types.InlineKeyboardButton(text="Yes", callback_data="yes")
-        no_button = types.InlineKeyboardButton(text="No", callback_data="no")
-        markup.add(yes_button, no_button)
-        
+    # Create a markup with Yes and No buttons
+    markup = types.InlineKeyboardMarkup()
+    yes_button = types.InlineKeyboardButton(text="Yes", callback_data="yes")
+    no_button = types.InlineKeyboardButton(text="No", callback_data="no")
+    markup.add(yes_button, no_button)
+    
+    if message.chat.type == 'photo':
+        result = time_from_photo(message.photo[-1])
         # Send a confirmation message with the markup
-        tl_bot.send_message(message.chat.id, f"Is it correct?", reply_markup=markup)
+        tg_bot.send_message(message.chat.id, f"Is {result} it correct?", reply_markup=markup)
+    elif contains_time(message.text):
+        # check if it's a time data
+        # saves it
+        # Otherwise continue
+        continue
+    else:
+        tl_bot.send_message(message.chat.id, "I don't understand it.")
+        ask_for_confirmation(message)
 
-@tl_bot.callback_query_handler(func=lambda call: True)
+@tg_bot.callback_query_handler(func=lambda call: True)
 def handle_callback_query(call):
     if call.data == "yes":
         # Save the data
         # save_to_sheets(data)
         # Edit message accordingly
-        tl_bot.answer_callback_query(call.id, "Success! ✅")
-        tl_bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Success! ✅", reply_markup=None)
+        tg_bot.answer_callback_query(call.id, "Success! ✅")
+        tg_bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Success! ✅", reply_markup=None)
     else:
         # Ask again?
-        tl_bot.answer_callback_query(call.id, "Please send the data again")
-        tl_bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Please send the data again", reply_markup=None)
+        tg_bot.answer_callback_query(call.id, "Please send the data again")
+        tg_bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Please send the data again", reply_markup=None)
 # Runs bot forever
-tl_bot.polling()
+tg_bot.polling()
 
 
 def elapsed_time(times_list):
@@ -67,3 +67,13 @@ def save_to_sheets(data):
 def special_cases():
     # For special days, when manual editing will be required
     pass
+
+def time_from_photo(img_obj):
+    # Open the image file
+    image = Image.open(img_obj)
+    # Extract text from the image
+    text = pytesseract.image_to_string(image)
+    # Look for time pattern (hh:mm)
+    time_pattern = re.compile(r'\b([01]?[0-9]|2[0-3]):[0-5][0-9]\b')
+    matches = time_pattern.findall(text)
+    return matches
