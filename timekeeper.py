1e0d1f8bff272870ddcf5610fa864c27d7e1cb87
Alexandre Stakonski <115736059+stakewalker@users.noreply.github.com>
2024-05-13 09:34:27 -0300

    Update timekeeper.py

--- timekeeper.py
+++ timekeeper.py
@@ -1,17 +1,12 @@
+import fns
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def main():
     # Keep listening to messages on Telegram group
-
-    # If an image is sent, convert its contents to text and filter the time (hh:mm)
-    # Otherwise if a message with "time" is received, use it instead
-
-    # Print the data and ask user to confirm
-
-    # Save this data to the Google Sheets file 
-
-    # If more than 4 registers are found in one day, calculate how many hours has passed 
-    
-    pass
+    while True:
+        fns.handle_message(message)
 
 if __name__ == "__main__":
     main()
