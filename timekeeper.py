dc701412f420a28d273c159edefe07ee1465c4a6
Alexandre Stakonski <115736059+stakewalker@users.noreply.github.com>
2024-06-20 17:19:21 -0300

    Update timekeeper.py

--- timekeeper.py
+++ timekeeper.py
@@ -2,6 +2,7 @@ import functions
 
 def main():
     # Keep listening to messages from user
+    print("TimeKeeper running!")
     while True:
         # Runs bot forever
         functions.tg_bot.polling()
